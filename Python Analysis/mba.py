1cc42b8ef6482f46a2807e6c932c07f05efd68c4
Aloysius <aloysiusng5@gmail.com>
2022-12-01 17:53:56 +0800

    mba

--- Python Analysis/mba.py
+++ Python Analysis/mba.py
@@ -4,7 +4,7 @@ from mlxtend.frequent_patterns import apriori
 from mlxtend.frequent_patterns import association_rules
 from datetime import datetime
 
-def generate_mba(df):
+def generate_mba(df, min_lift=2, min_confidence=0.5, min_support=0.05):
 
     print("starting market basket analysis......")
     start = datetime.now()
@@ -17,7 +17,7 @@ def generate_mba(df):
 
     # create frequent item sets
     # rules = minimum support 5%
-    print("creating frequent itemsets with minimum support of 5%....")
+    print(f"creating frequent itemsets with minimum support of {min_support*100}%....")
     frequent_itemsets  = apriori(basket_df, min_support=0.05, use_colnames=True)
     frequent_itemsets['length'] = frequent_itemsets['itemsets'].apply(lambda x: len(x))
     print(frequent_itemsets)
@@ -28,9 +28,9 @@ def generate_mba(df):
     print(rules)
 
     # clean rules
-    print("further cleaning of association rules [confidence >= 0.5, lift >= 1.5]....")
-    rules = rules[rules['confidence'] >= 0.5]
-    rules = rules[rules['lift'] >= 1.5]
+    print(f"further cleaning of association rules [confidence >= {min_confidence}, lift >= {min_confidence}]....")
+    rules = rules[rules['confidence'] >= min_confidence]
+    rules = rules[rules['lift'] >= min_lift]
 
     #  Preparing clean data for Tableau (from frozenset to string) 
     print("formatting data....")
@@ -48,13 +48,13 @@ def generate_mba(df):
 
     # mapping rules to relevant information.... 
     print("mapping rules to relevant information....")
-    code_to_name_dict = pd.Series(df["Product Name TH"].values, index=df["Product Code"]).to_dict()
-    code_to_group_level1_dict = pd.Series(df["GroupNameLevel1"].values, index=df["Product Code"]).to_dict()
-    code_to_group_level2_dict = pd.Series(df["GroupNameLevel2"].values, index=df["Product Code"]).to_dict()
+    code_to_name_dict = pd.Series(df["ProductNameTH"].values, index=df["ProductCode"]).to_dict()
+    code_to_group_level1_dict = pd.Series(df["GroupNameLevel1"].values, index=df["ProductCode"]).to_dict()
+    code_to_group_level2_dict = pd.Series(df["GroupNameLevel2"].values, index=df["ProductCode"]).to_dict()
     code_to_name_dict = {str(key): str(value) for key, value in code_to_name_dict.items()}
     code_to_group_level1_dict = {str(key): str(value) for key, value in code_to_group_level1_dict.items()}
     code_to_group_level2_dict = {str(key): str(value) for key, value in code_to_group_level2_dict.items()}
-    total_revenue_pdt_code_dict = df.groupby(['Product Code']).Revenue.sum().to_dict()
+    total_revenue_pdt_code_dict = df.groupby(['ProductCode']).NetAmount.sum().to_dict()
     total_revenue_pdt_code_dict = {str(key): str(round(value, 2)) for key, value in total_revenue_pdt_code_dict.items()}
 
     
@@ -87,6 +87,16 @@ def generate_mba(df):
     print(rules)
 
     rules.reset_index(inplace=True, drop=True)
+    # find coluumn number by name
+    print("Splitting columns ...")
+    # split a column of string to multiple columns
+    def splitCol(df, colName):
+        return df.join(df[colName].str.split(',', expand=True).rename(columns={0:colName+"0", 1:colName+"1", 2:colName+"2", 3:colName+"3", 4:colName+"4", 5:colName+"5", 6:colName+"6"}))
+
+    cols_to_split = ["antecedents", "ant_thai", "ant_grouplevel1", "ant_grouplevel2", "ant_pdt_ttl_net_amt", "consequents", "con_thai", "con_grouplevel1", "con_grouplevel2", "con_pdt_ttl_net_amt"]
+    for i in cols_to_split:
+        rules[i] = rules[i].astype(str)
+        rules = splitCol(rules, i)
 
     # export rules to csv
     print("exporting association rules to CSV....")
