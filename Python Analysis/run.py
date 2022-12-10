b599d4cb57a668b95646b8d0928388bf83c954a6
Wei Khiang <weikhiang92000@gmail.com>
2022-12-10 17:46:22 +0700

    recommend top 10 instead of 5

--- Python Analysis/run.py
+++ Python Analysis/run.py
@@ -8,24 +8,24 @@ print("starting batch process......")
 start = datetime.now()
 
 print("loading transaction data......")
-transaction_file_name = "../obd_transaction.csv"
+transaction_file_name = "../ETL/CSV/obd_transaction.csv"
 df = pd.read_csv(transaction_file_name, index_col=0)
 
 print("transaction data loaded")
 print(df)
 
 generate_mba(df, 2, 0.5, 0.05) # df, min_lift, min_confidence, min_support
-# generate_recommender(df)
+generate_recommender(df)
 
-# print("loading customer data......")
-# customer_file_name = "../ETL/CSV/obd_customer_unique.csv"
-# df_customer = pd.read_csv(customer_file_name, index_col=0)
-# print(df_customer)
+print("loading customer data......")
+customer_file_name = "../ETL/CSV/obd_customer_unique.csv"
+df_customer = pd.read_csv(customer_file_name, index_col=0).dropna(subset=['Latitude', 'Longitude'])
+print(df_customer)
 
-# print("customer data loaded")
-# print("starting route optimization......")
-# transaction_df = df.drop_duplicates(subset=['CustomerCode', 'ProductCode'], keep='first')
-# generate_clusters(df_customer, transaction_df)
+print("customer data loaded")
+print("starting route optimization......")
+transaction_df = df.drop_duplicates(subset=['CustomerCode', 'ProductCode'], keep='first').reset_index(drop=True)
+generate_clusters(df_customer, transaction_df)
 
 end = datetime.now()
 print( "Total time taken for batch run: ", end - start )
